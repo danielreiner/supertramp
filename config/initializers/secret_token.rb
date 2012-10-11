4f8be8adc2ecaed14611f818a51e5978c4da9506
David Reissner <david@sauspiel.de>
2012-10-11 18:45:00 +0200

    Add rails project to supertramp repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supertramp::Application.config.secret_token = '3b90d1d573e643eb209b3aab1cbf55bad06c6b37ef45e793e66c661fca06a0ecbebede5b05024e48b8ee5b664a4b39a9985c7c585ea08869e32b7b1d3f2d1a72'
