29a97e8bd9cc69293dfccb104dff6b3fdab13230
David Reissner <david@sauspiel.de>
2012-10-21 17:14:44 +0200

    More work on messaging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,13 @@ Supertramp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'cryptic-bastion-8253.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://cryptic-bastion-8253.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
