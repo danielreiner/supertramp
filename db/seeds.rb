2e646cb46f701972a8e506512ea5dee573d9d4b0
David Reissner <david@sauspiel.de>
2012-11-26 18:38:29 +0100

    Add fabian as second default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'David', :email => 'cliffsworld@gmail.com', :password => '4x4ist16', :password_confirmation => '4x4ist16'
+user = User.create! :name => 'David', :email => 'cliffsworld@gmail.com', :password => '4x4ist8', :password_confirmation => '4x4ist8'
+user.confirm!
+puts 'New user created: ' << user.name
+user.add_role :admin
+
+puts 'SETTING UP SECOND DEFAULT USER LOGIN'
+user = User.create! :name => 'Fabian', :email => 'f.trinler@googlemail.com', :password => '1x1ist2', :password_confirmation => '1x1ist2'
 user.confirm!
 puts 'New user created: ' << user.name
 user.add_role :admin
